0f59b00a089e78e14aabb8308c13ac3b04db37a2
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-12-25 23:22:16 +0100

    added js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,71 @@
 'use strict';
 
 // Selecting elements
-const score0El = document.querySelector('#score--0')
-const score1El = document.getElementById('score--1')
-const diceEl = document.querySelector('.dice')
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
-score0El.textContent = 0
-score1El.textContent = 0
-diceEl.classList.add('hidden')
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function(){
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+ 
+}
+
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    //  Add dice to the current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch to next player
+    switchPlayer()
+     }
+});
+
+btnHold.addEventListener('click', function(){
+    // 1. add current score to active player's score
+    scores[activePlayer] += currentScore
+    // score[1] = score[1] + currentScore
+    document.getElementById(`score--${activePlayer}`). textContent = scores[activePlayer]
+
+    // Check if player's score is >= 100
+    // Finish the game
+
+
+    // Switch to the next player
+    switchPlayer()
+
+
+})

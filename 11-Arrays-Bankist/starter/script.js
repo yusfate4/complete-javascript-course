604e9aa54516e94145862e88a0cbca5e91031d03
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-02-09 22:00:31 +0100

    new js

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,29 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function(movements){
-movements.forEach(function(mov, index))
-}
-displayMovements(account1.movements)
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+  <div class="movements__row">  <div class="movements__type movements__type--$
+  {type}">${i + 1} ${type}</div>
+  <div class="movements__value">${mov}</div>
+</div> 
+  `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+displayMovements(account1.movements);
+
+const user = 'Steven Thomas Williams';
+const username = user.toLowerCase().split(' ')
+
+console.log(username);
+
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -115,7 +134,6 @@ console.log(arr.at(0));
 console.log(arr[arr.length - 1]);
 console.log(arr.slice(-1)[0]);
 console.log(arr.at(-1));
-*/
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 for (const [i, movement] of movements.entries()) {
@@ -149,7 +167,89 @@ currencies1.forEach(function (value, key, map) {
 // SET
 const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
 console.log(currenciesUnique);
-currenciesUnique.forEach(function(value, _, map){
+currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
+});
 
-})
+// Coding Challenge #1
+*/
+
+/* 
+Julia and Kate are doing a study on dogs. 
+So each of them asked 5 dog owners about their dog's age, 
+and stored the data into an array (one array for each). 
+For now, they are just interested in knowing whether a dog
+ is an adult or a puppy. A dog is an adult if it is at least 
+ 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of 
+dog's ages ('dogsJulia' and 'dogsKate'), and does the
+ following things:
+
+1. Julia found out that the owners of the FIRST 
+and the LAST TWO dogs actually have cats, not dogs! 
+So create a shallow copy of Julia's array, and remove 
+ cat ages from that copied array (because it's a bad 
+ practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and 
+Kate's data
+3. For each remaining dog, log to the console whether 
+it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+/*
+function checkDogs(dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  });
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+*/
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+// const movementsUSD =  movements.map(function (mov){
+//   return eurToUsd *  mov
+// })
+
+// console.log(movements);
+// console.log(movementsUSD);
+
+// Arrow function
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map((mov, i, arr) => {
+  if (mov > 0) {
+    return `Movement ${i + 1}: You deposited ${mov}`;
+  } else {
+    return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  }
+});
+
+console.log(movementsDescriptions);

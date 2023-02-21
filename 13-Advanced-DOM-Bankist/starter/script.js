8e93b96e5c147a6a7693533439a8c43ba2d55389
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-02-21 22:37:56 +0100

    updated the js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,9 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
-
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,3 +28,70 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// lecture
+// selecting elements
+const header = document.querySelector('header');
+const allSections = document.querySelectorAll('section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+
+message.innerHTML =
+  'We use cookied for improved functionality and analytics. <button class = btn btn--close-cookie">Got it! </button>';
+
+// header.prepend(message)
+header.append(message);
+
+// header.before(message)
+// header.after(message)
+
+// Delete elements
+// document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+//   message.remove()
+// })
+
+// Style, attribute and classes
+
+// Style
+message.style.backgroundColor = '#37383d'
+message.style.width = '1200'
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px'
+
+
+document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+
+// Attribute
+const logo = document.querySelector('.nav__logo')
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.getAnimations('src'));
+
+const link = document.querySelector('.twitter-link')
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Classes
+logo.classList.add('c')
+logo.classList.remove('c')
+logo.classList.toggle('c')
+logo.classList.contains('c')
+
+

6ad642d9277399180b1defaf36ad589a36447ad6
Dahud Yusuf <abiodunyusuf4@gmail.com>
2023-02-24 21:15:15 +0100

    js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,18 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -20,6 +26,7 @@ const closeModal = function () {
 };
 
 btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
@@ -29,69 +36,119 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// lecture
-// selecting elements
-const header = document.querySelector('header');
-const allSections = document.querySelectorAll('section');
-console.log(allSections);
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
 
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
+  console.log(e.target.getBoundingClientRect());
 
-console.log(document.getElementsByClassName('btn'));
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
 
-// creating and inserting elements
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
 
-message.innerHTML =
-  'We use cookied for improved functionality and analytics. <button class = btn btn--close-cookie">Got it! </button>';
+  // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
 
-// header.prepend(message)
-header.append(message);
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
 
-// header.before(message)
-// header.after(message)
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-// Delete elements
-// document.querySelector('.btn--close-cookie').addEventListener('click', function(){
-//   message.remove()
-// })
+// Page navigation
 
-// Style, attribute and classes
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
 
-// Style
-message.style.backgroundColor = '#37383d'
-message.style.width = '1200'
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
 
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
+document.querySelector('nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
 
-message.style.height =
-Number.parseFloat(getComputedStyle(message).height, 10) + 40 + 'px'
+/*
+// DOM traversing
+const h1 = document.querySelector('h1');
+
+// Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
 
 
-document.documentElement.style.setProperty('--color-primary', 'orangered')
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+*/
+///////////////////////////////////////
+/*
+
+// Event
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.addEventListener('mouseenter', alertH1), 3000);
+
+// Event propagation
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
 
 
-// Attribute
-const logo = document.querySelector('.nav__logo')
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.getAnimations('src'));
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
 
-const link = document.querySelector('.twitter-link')
-console.log(link.href);
-console.log(link.getAttribute('href'));
-
-// Classes
-logo.classList.add('c')
-logo.classList.remove('c')
-logo.classList.toggle('c')
-logo.classList.contains('c')
-
-
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+*/
